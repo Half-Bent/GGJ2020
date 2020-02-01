02f3fdac0a59564e8c5af5c951d6b0c1456ae349
Marko <m.j.niiranen@gmail.com>
2020-02-01 11:22:11 +0200

    Add Player animations

---
+++ GGJ_2020/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator myAnim;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void AnimateMovement(Vector3 movement)
+    {
+        spriteRenderer.flipX = movement.x < 0 ? true : false;
+
+        myAnim.SetFloat("xSpeed", movement.x);
+        myAnim.SetFloat("ySpeed", movement.y);
+    }
+
+    public void AnimateRepair()
+    {
+        myAnim.SetTrigger("repair");
+    }
+}
