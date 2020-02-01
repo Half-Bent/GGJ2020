af725ebd1b3fa9ef0a0e9f7033266a51464dae9e
Anton <noriss@kapsi.fi>
2020-02-01 14:13:33 +0200

    game start

---
+++ GGJ_2020/Assets/Scripts/UI/GameStart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public UpperManager manager;
+    public GameObject Player1;
+    public GameObject player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(bool Solo){
+        if(Solo){
+            player2.SetActive(false);
+        }
+
+        manager.started = true;
+    }
+}
